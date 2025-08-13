ba7e7cfa7c58546882d0021f4e0f97292cb26be8
Ademar Santiago <67010728+ademar-santiago@users.noreply.github.com>
2025-08-13 20:41:30 +0000

    Exibição das cartas na tela Terminal.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -31,5 +31,30 @@ int main() {
     float pibPerCapita1 = (pib1 * 1000000000.0) / populacao1; 
     float pibPerCapita2 = (pib2 * 1000000000.0) / populacao2;
 
+
+     // Exibição das cartas na tela Terminal.
+ 
+    printf("Carta 1:\n");
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per capita: %.2f reais\n", pibPerCapita1);
+
+    printf("\nCarta 2:\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per capita: %.2f reais\n", pibPerCapita2);
+
     return 0;
 }
